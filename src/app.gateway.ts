52029bc9df7cd3b7cbf6da462bc63c37154ad79f
brenosgomes <breno_g_@hotmail.com>
2022-03-13 15:37:54 -0300

    first commit

---
+++ src/app.gateway.ts
@@ -0,0 +1,47 @@
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WsResponse,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+
+@WebSocketGateway({ namespace: '/chat' })
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+  public message = [];
+
+  afterInit(server: any) {
+    this.logger.log('Inicialized');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+  handleConnection(client: any, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  @SubscribeMessage('messageServer')
+  handleMessage(
+    client: Socket,
+    payload: { sender: string; message: string; likes: number },
+  ) {
+    this.message.push(payload);
+    this.server.emit('messageClient', this.message);
+  }
+
+  @SubscribeMessage('likeMessageServer')
+  handleLike(client: Socket, payload: { id: number }) {
+    this.message[payload.id].likes++;
+    this.server.emit('messageClient', this.message);
+  }
+}
